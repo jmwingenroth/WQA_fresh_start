c920f182be15bd49827f4f80bec039fde6271924
jmwingenroth <jwing421@gmail.com>
2025-10-17 13:07:39 -0700

    Clean up understand_filter.r

--- src/debug/understand_filter.r
+++ src/debug/understand_filter.r
@@ -24,47 +24,33 @@ pre %>%
   as_tibble() %>%
   distinct(MonitoringLocationIdentifier) %>%
   nrow()
-
-# 143032
-
-pre %>%
-  as_tibble() %>%
-  filter_wqp() %>%
-  filter(MonitoringLocationIdentifier %in% nitro_tidy$MLI) %>%
-  as_tibble() %>%
-  distinct(MonitoringLocationIdentifier) %>%
-  nrow()
-
-# 39634
-
-pre %>%
-  group_by(CharacteristicName,ResultMeasure.MeasureUnitCode,ResultSampleFractionText) %>%
-  tally() %>%
-  arrange(desc(n))
-
-# Filter out concentrations < or = 0
-# 36203
+# Sites in wqp_pull.fst: 143032
 
 pre %>%
   as_tibble() %>%
   filter_wqp() %>%
-  filter(MonitoringLocationIdentifier %in% nitro_tidy$MLI) %>%
   as_tibble() %>%
   distinct(MonitoringLocationIdentifier) %>%
   nrow()
+# Sites after filtering with criteria in src/lib/wqp_filter.r : 39634
 
-# 25732
+pre %>%
+  as_tibble() %>%
+  filter_wqp() %>%
+  filter(ResultMeasureValue > 0) %>%
+  as_tibble() %>%
+  distinct(MonitoringLocationIdentifier) %>%
+  nrow()
+# Sites after dropping 0 (and negative) measurements: 36203
 
 post %>%
   as_tibble() %>%
   distinct(MLI) %>%
   nrow()
-
-# 25732
+# Sites in nitrogen_mass_by_MLI copy.fst: 25732
 
 new_vars %>%
   as_tibble() %>%
   distinct(MLI) %>%
   nrow()
-
-# 25732
+# Sites in new_vars_by_MLI.fst: 25732
